cb515144f842d968615d00187acd88eb2c0ce428
lalatina11 <rahmadancandra@gmail.com>
2026-01-17 11:24:52 +0700

    feat: api for checking health

--- src/app.ts
+++ src/app.ts
@@ -2,4 +2,8 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (_req, res) => {
+  res.json({ error: false, message: "OK", date: new Date(Date.now()) });
+});
+
 export default app;
