ea3e7f3127da6b8560f2f171158a8f358b2dd881
lalatina11 <rahmadancandra@gmail.com>
2026-01-17 11:23:12 +0700

    feat: running express app

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log("Hello via Bun!");
+import app from "./src/app";
+import { connectDB } from "./src/config/db";
+import ENV from "./src/config/env";
+
+app.listen(ENV.PORT, () => {
+  connectDB();
+  console.log(`Server Up and Running on http://localhost:${ENV.PORT}`);
+});
