7407c5eddeaa2948524a4614e017741273387067
lalatina11 <rahmadancandra@gmail.com>
2026-01-17 18:51:15 +0700

    refactor: formatting by biome

--- src/router/auth.ts
+++ src/router/auth.ts
@@ -3,7 +3,7 @@ import { Router } from "express";
 const authRouter = Router();
 
 authRouter.get("/me", (_req, res) => {
-  res.status(200).json({ error: false, message: "OK", data: {} });
+	res.status(200).json({ error: false, message: "OK", data: {} });
 });
 
 export default authRouter;
