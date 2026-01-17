b6e525d2fb3c9bfb9fc40b9c424ed6db8b74ec0a
lalatina11 <rahmadancandra@gmail.com>
2026-01-17 18:50:29 +0700

    feat: apply all routes

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,24 @@
+import cors from "cors";
 import express from "express";
+import authRouter from "./router/auth";
+import chatRouter from "./router/chat";
+import messageRouter from "./router/message";
+import userRouter from "./router/user";
 
 const app = express();
 
+app.use(express.json());
+app.use(cors());
+
 app.get("/", (_req, res) => {
-  res
-    .status(200)
-    .json({ error: false, message: "OK", date: new Date(Date.now()) });
+	res
+		.status(200)
+		.json({ error: false, message: "OK", date: new Date(Date.now()) });
 });
+
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+app.use("/api/chat", chatRouter);
+app.use("/api/message", messageRouter);
 
 export default app;
