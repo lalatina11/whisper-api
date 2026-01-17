cfd0d7e4c8e067cc7eb45b36dd948f08256ff778
lalatina11 <rahmadancandra@gmail.com>
2026-01-17 18:07:06 +0700

    feat: adding status code for testing api

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,9 @@ import express from "express";
 const app = express();
 
 app.get("/", (_req, res) => {
-  res.json({ error: false, message: "OK", date: new Date(Date.now()) });
+  res
+    .status(200)
+    .json({ error: false, message: "OK", date: new Date(Date.now()) });
 });
 
 export default app;
