c55536ce38923bbe13663985a941d150b7a62860
lalatina11 <rahmadancandra@gmail.com>
2026-01-17 18:15:19 +0700

    refactor: biome format

--- index.ts
+++ index.ts
@@ -3,6 +3,6 @@ import { connectDB } from "./src/config/db";
 import ENV from "./src/config/env";
 
 app.listen(ENV.PORT, () => {
-  connectDB();
-  console.log(`Server Up and Running on http://localhost:${ENV.PORT}`);
+	connectDB();
+	console.log(`Server Up and Running on http://localhost:${ENV.PORT}`);
 });
