dabd47035b0abeaf44d337e89970527bfd12cdb7
lalatina11 <rahmadancandra@gmail.com>
2026-01-17 18:15:12 +0700

    refactor: biome format

--- src/config/db/index.ts
+++ src/config/db/index.ts
@@ -2,11 +2,11 @@ import mongoose from "mongoose";
 import ENV from "../env";
 
 export const connectDB = async () => {
-  try {
-    await mongoose.connect(ENV.DATABASE_URL);
-    console.log("MongoDB connected");
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
+	try {
+		await mongoose.connect(ENV.DATABASE_URL);
+		console.log("MongoDB connected");
+	} catch (error) {
+		console.log(error);
+		process.exit(1);
+	}
 };
