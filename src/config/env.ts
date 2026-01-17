9c5c37d00e9998fa2aa8fe7c594723e2a29385b7
lalatina11 <rahmadancandra@gmail.com>
2026-01-17 18:50:41 +0700

    refactor: formatting by biome

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,24 +2,24 @@ import z from "zod";
 import "dotenv/config";
 
 const envSchema = z.object({
-  NODE_ENV: z
-    .enum(["production", "development", "stagging"])
-    .default("development"),
-  DATABASE_URL: z.string().min(1),
-  PORT: z.coerce.number().min(1000),
+	NODE_ENV: z
+		.enum(["production", "development", "stagging"])
+		.default("development"),
+	DATABASE_URL: z.string().min(1),
+	PORT: z.coerce.number().min(1000),
 });
 
 const getEnv = () => {
-  try {
-    const { success, data } = envSchema.safeParse(process.env);
-    if (!success) {
-      throw new Error("Cannot find the .env or some env variables!");
-    }
-    return data;
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
+	try {
+		const { success, data } = envSchema.safeParse(process.env);
+		if (!success) {
+			throw new Error("Cannot find the .env or some env variables!");
+		}
+		return data;
+	} catch (error) {
+		console.log(error);
+		process.exit(1);
+	}
 };
 
 const ENV = getEnv();
