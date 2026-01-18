57d6798eabfd78ea57d444d1054a2c3abcd577b9
lalatina11 <rahmadancandra@gmail.com>
2026-01-18 08:58:29 +0700

    feat: auth handler - auth handler - auth controller - error handler - global types - auth request custom

---
+++ src/controllers/auth.ts
@@ -0,0 +1,67 @@
+import { clerkClient, getAuth } from "@clerk/express";
+import type { NextFunction, Request, Response } from "express";
+import UserModel from "../models/user";
+import type { AuthRequest } from "../types";
+
+export const getCurrentUser = async (
+	req: AuthRequest,
+	res: Response,
+	next: NextFunction,
+) => {
+	try {
+		const id = req.userId;
+		if (!id) {
+			return res.status(401).json({ error: true, message: "Unauthorized!" });
+		}
+		const user = await UserModel.findById(id);
+
+		if (!user) {
+			return res
+				.status(404)
+				.json({ error: true, message: "Cannon found user!" });
+		}
+		return res
+			.status(200)
+			.json({ error: false, message: "OK", data: user.toObject() });
+	} catch (error) {
+		console.log(error);
+		res.status(500);
+		next();
+	}
+};
+
+export const authCallback = async (
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) => {
+	try {
+		const { userId: clerkId } = getAuth(req);
+		if (!clerkId) {
+			return res.status(401).json({ error: true, message: "Unauthorized!" });
+		}
+
+		let user = await UserModel.findOne({ clerkId });
+
+		if (!user) {
+			const clerkUser = await clerkClient.users.getUser(clerkId);
+			if (!clerkUser) {
+				return res.status(401).json({ error: true, message: "Unauthorized!" });
+			}
+			user = await UserModel.create({
+				clerkId: clerkUser.id,
+				email: clerkUser.primaryEmailAddress?.emailAddress,
+				name:
+					clerkUser.fullName ??
+					clerkUser.primaryEmailAddress?.emailAddress.split("@")[0],
+				avatar: clerkUser.imageUrl || "",
+			});
+			await user.save();
+		}
+		return res.status(200).json({ error: false, message: "OK", data: user });
+	} catch (error) {
+		console.log(error);
+		res.status(500);
+		next();
+	}
+};
