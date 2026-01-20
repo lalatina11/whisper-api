de0858ac5afd2dd626d01dc3283cc1200a8c7305
lalatina11 <rahmadancandra@gmail.com>
2026-01-20 20:31:06 +0700

    refactor(feat): listening server together with socket

--- index.ts
+++ index.ts
@@ -1,10 +1,16 @@
+import { createServer } from "node:http";
 import app from "./src/app";
 import { connectDB } from "./src/config/db";
 import ENV from "./src/config/env";
+import { initializeSocket } from "./src/utils/socket";
+
+const httpServer = createServer(app);
+
+initializeSocket(httpServer);
 
 connectDB()
 	.then(() => {
-		app.listen(ENV.PORT, () => {
+		httpServer.listen(ENV.PORT, () => {
 			console.log(`Server Up and Running on http://localhost:${ENV.PORT}`);
 		});
 	})
